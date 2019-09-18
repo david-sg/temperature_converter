b400bb9c06a897b87198894cf246e6b76f338138
David M <davidmosk@gmail.com>
2019-09-18 19:05:50 +0800

    working version with celcius and kelvin

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
+  var currentInputNumber = Number( currentInput );	
   console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+    if (isNaN(currentInputNumber))
+	{
+	    return "Please enter a number";
+	}
+  // var output = "WOW TEMPERATURE";
+  // calculate temp in celcius and kelvin
+  var output = currentInput + "℉ is: " + calculateCelcius(currentInputNumber) + "℃ and in Kelvin is: " + calculateKelvin(currentInputNumber); 
   return output;
 };
+
+var calculateCelcius = function (currentInputNumber) {
+var	celcius = (5/9) * (currentInputNumber - 32);
+celcius = celcius.toFixed(2);
+	return celcius; 
+}
+
+var calculateKelvin = function (currentInputNumber) {
+var kelvin = ((currentInputNumber - 32)/1.8)+273.15;
+kelvin = kelvin.toFixed(2);
+	return kelvin; 
+}
