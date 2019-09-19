cbe44546273fa318b00c9ceda55e35827913ec85
David M <davidmosk@gmail.com>
2019-09-19 15:29:50 +0800

    added steps and asks for F C or K, checks if F entered, gives output

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 console.log("hello script js");
 
+var step = 0;
+var output = "";
+var storeNumber = "";
+var userType = "";
+
 var inputHappened = function(currentInput){
+  if (step ===0) {
   var currentInputNumber = Number( currentInput );	
   console.log( currentInput );
     if (isNaN(currentInputNumber))
 	{
 	    return "Please enter a number";
 	}
-  // var output = "WOW TEMPERATURE";
-  // calculate temp in celcius and kelvin
-  var output = currentInput + "℉ is: " + calculateCelcius(currentInputNumber) + "℃ and in Kelvin is: " + calculateKelvin(currentInputNumber); 
+  storeNumber = currentInputNumber;
+  step = step +1;
+  return( "Is this F, C, or K?" )
+  } else if (step ===1) {
+  if (currentInput ==="F"){
+    userType = "F";
+  output = currentInput + "℉ is: " + calculateCelcius(storeNumber) + "℃ and in Kelvin is: " + calculateKelvin(storeNumber); 
   return output;
-};
+  } else if (currentInput ==="C"){
+    //convert to F and K
+        userType = "C";
+  } else if (currentInput==="K"){
+        userType = "K";
+    //convert to C and F
+  }
+
+  else {
+    return "Is this F, C, or K?";
+  }
+}
+
+    }
 
 var calculateCelcius = function (currentInputNumber) {
 var	celcius = (5/9) * (currentInputNumber - 32);
